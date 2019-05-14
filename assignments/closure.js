671ad00308d839a5468a1e184a9e7a3ce151ee4f
Michael Baynon <baynonmichael@gmail.com>
2019-05-14 18:48:53 -0400

    up to date file done with closures and array methods but callbacks still needs some work

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,13 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+let king = 'henry';
+
+function myFunction() {
+  return king;
+}
+myFunction();
+console.log(myFunction);
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
